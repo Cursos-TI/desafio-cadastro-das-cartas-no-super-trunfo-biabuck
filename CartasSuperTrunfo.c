63d13e8d09de2fe9743c86b7bc8730f1c1bf5b70
biabuck <beatrizbuck300@gmail.com>
2025-03-12 16:46:02 +0000

    Super Trunfo Lógica (comparação)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-    int main() {
-        printf("***Desafio Super Trunfo*** \n");
-        srand(time(0));
+int main() {
+    printf("***Desafio Super Trunfo*** \n");
+    srand(time(0));
 
         // Carta 1
         char estado1[3] = "A"; 
@@ -17,11 +17,13 @@
         float densidadePopulacional1;
         float PIBperCapta1;
         int escolhaJogador;
+        float valorJogador;
+        char *atributo1;
 
         densidadePopulacional1 = populacao1 / area1;
         PIBperCapta1 = PIB1 / populacao1;
 
-        // Carta 2
+    // Carta 2
         char estado2[3] = "B"; 
         char codigo2[4] = "B02"; 
         char cidade2[50] = "Rio de Janeiro";
@@ -31,14 +33,18 @@
         int pontosTuristicos2 = 30;
         float densidadePopulacional2;
         float PIBperCapta2;
-        int escolhaComputador = rand() % 5 + 1;
-    
+        int escolhaComputador;
+        float valorComputador;
+        char *atributo2;
 
         PIBperCapta2 = PIB2 / populacao2;
         densidadePopulacional2 = populacao2 / area2;
 
+        //Escolha aleatória
+        escolhaComputador = rand() % 5 + 1;
+
+        // Opções jogador
         printf("Escolha uma opção: \n");
-        printf("Cidade: %s \n", cidade1);
         printf("1. População: %u \n", populacao1);
         printf("2. Área: %.2f km \n", area1);
         printf("3. PIB: %.2f bilhões de reais \n", PIB1);
@@ -47,112 +53,86 @@
         printf("Escolha: ");
         scanf("%d", &escolhaJogador);
 
-
-        switch (escolhaJogador)
-    {
-    case 1:
-        printf("Cidade: %s \n", cidade1);
-        printf("População: %u \n", populacao1);
-        break;
-
-    case 2:
-        printf("Cidade: %s \n", cidade1);
-        printf ("Área: %.2f \n", area1);
-        break;
-    
-    case 3:
-        printf("Cidade: %s \n", cidade1);
-        printf("PIB: %.2f \n", PIB1);
-        break;
-
-    case 4:
-        printf("Cidade: %s \n", cidade1);
-        printf("Pontos turísticos: %d \n", pontosTuristicos1);
-        break;
-
-    case 5:
-        printf("Cidade: %s \n", cidade1);
-        printf("Densidade Populacional: %.2f \n", densidadePopulacional1);
-        break;
-    
-    default:
-        printf("opção inválida");
-        break;
-    }
-
-    switch (escolhaComputador)
-    {
-        case 1:
-        printf("Cidade: %s \n", cidade2);
-        printf("População: %u \n", populacao2);
-
-        if(escolhaJogador > escolhaComputador)
+       
+        switch (escolhaJogador) 
         {
-            printf("Você ganhou \n");
-        } else if (escolhaJogador < escolhaComputador)
-        {
-           printf("Você perdeu \n");
+            case 1:
+                valorJogador = populacao1;
+                atributo1 = "População";
+                break;
+
+            case 2:
+                valorJogador = area1;
+                atributo1 = "Área";
+                break;
+
+            case 3:
+                valorJogador = PIB1;
+                atributo1 = "PIB";
+                break;
+
+            case 4:
+                valorJogador = pontosTuristicos1;
+                atributo1 = "Pontos turísticos";
+                break;
+
+            case 5:
+                valorJogador = densidadePopulacional1;
+                atributo1 = "Densidade Populacional";
+                break;
+
+            default:
+                printf("Opção inválida\n");
         }
-        break;
-        
 
-    case 2:
-        printf("Cidade: %s \n", cidade2);
-        printf ("Área: %.2f \n", area2);
-        break;
 
-        if(escolhaJogador > escolhaComputador)
+        switch (escolhaComputador) 
         {
-            printf("Você ganhou \n");
-        } else if (escolhaJogador < escolhaComputador)
-        {
-           printf("Você perdeu \n");
+            case 1:
+                valorComputador = populacao2;
+                atributo2 = "População";
+                break;
+            case 2:
+                valorComputador = area2;
+                atributo2 = "Área";
+                break;
+            case 3:
+                valorComputador = PIB2;
+                atributo2 = "PIB";
+                break;
+            case 4:
+                valorComputador = pontosTuristicos2;
+                atributo2 = "Pontos turísticos";
+                break;
+            case 5:
+                valorComputador = densidadePopulacional2;
+                atributo2 = "Densidade Populacional";
+                break;
         }
-    
-    case 3:
+
+        //Escolhas
+        printf("Cidade: %s \n", cidade1);
+        printf("%s: %.2f \n", atributo1, valorJogador);
         printf("Cidade: %s \n", cidade2);
-        printf("PIB: %.2f \n", PIB2);
+        printf("%s: %.2f \n", atributo2, valorComputador);
 
-        if(escolhaJogador > escolhaComputador)
-        {
-            printf("Você ganhou \n");
-        } else if (escolhaJogador < escolhaComputador)
-        {
-           printf("Você perdeu \n");
+        // Comparação de valores
+        if (escolhaJogador == 5)  //Densidade populacional
+        { 
+            if (valorJogador < valorComputador) 
+            {
+                printf("Parabéns, você ganhou!!! \n");
+            } else if (valorJogador > valorComputador) 
+            {
+                printf("Você perdeu \n");
+            } 
+
+        } else {
+            if (valorJogador > valorComputador) {
+                printf("Parabéns, você ganhou!!! \n");
+            } else if (valorJogador < valorComputador) {
+                printf("Você perdeu \n");
+            }
         }
-        break;
-
-    case 4:
-        printf("Cidade: %s \n", cidade2);
-        printf("Pontos turísticos: %d \n", pontosTuristicos2);
-
-        if(escolhaJogador > escolhaComputador)
-        {
-            printf("Você ganhou \n");
-        } else if (escolhaJogador < escolhaComputador)
-        {
-           printf("Você perdeu \n");
-        }
-        break;
-
-    case 5:
-        printf("Cidade: %s \n", cidade2);
-        printf("Densidade Populacional: %.2f \n", densidadePopulacional2);
-
-        if (escolhaJogador < escolhaComputador)
-        {
-            printf("Você ganhou \n");
-        } else if (escolhaJogador > escolhaComputador)
-        {
-           printf("Você perdeu \n");
-        }
-        break;
-    
-    default:
-        
-        break;
-    
-    }
-  
-    return 0;
+        return 0;
 }
